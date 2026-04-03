d5d48e5dd35847ab038ed35f45b176476df8ea4f
Abhishek <abhishekkumar12ka4@gmail.com>
2026-04-03 18:29:25 +0530

    Update

--- quickmedi-web/next.config.ts
+++ quickmedi-web/next.config.ts
@@ -4,11 +4,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  allowedDevOrigins: [
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'https://api-lke8.onrender.com/'
-  ],
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+    experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "https://api-lke8.onrender.com/", "http://localhost:5000"],
+    },
+  },
 };
 
 export default nextConfig;
